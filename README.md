7c3256a0105757fab6a98745227af6758e80f514
BeaPerea <trizy_45@hotmail.com>
2024-01-22 19:08:38 +0100

    Folder Synchronization

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# folder-synchronization
- Developed in Python, offers a solution for keeping two folders synchronized across different storage locations
+# Folder Synchronization
+
+## Description
+Folder Synchronization is a Python script designed to synchronize files and folders between a source directory and a replica directory. The script continuously monitors the source directory and replicates its contents to the replica directory, ensuring both directories are kept in sync. It handles the creation, deletion, and updating of files and folders, and logs all actions performed during synchronization.
+
+## Features
+- **Directory Synchronization**: Synchronizes all files and folders from a source directory to a replica directory.
+- **Continuous Monitoring**: Runs in a loop, checking for changes at specified intervals.
+- **Logging**: Logs all synchronization activities to a specified log file.
+- **File Content Verification**: Uses MD5 checksums to verify file content and ensure accurate replication.
+
+## Requirements
+- Python 3.x
+
+## Installation
+Clone the repository or download the script to your local machine.
+
+## Usage
+To use the Folder Synchronization, run the script from the command line with the following arguments:
+
+```bash
+python folderSynchronization.py <source_directory> <replica_directory> <interval_in_minutes> <log_file_path>
---
+++ folderSynchronization.py
@@ -0,0 +1,128 @@
+import argparse
+import os
+import sys
+import shutil
+import hashlib
+import time
+
+def controlArguments():
+    global pathSource, pathReplica, fileToLog
+    parser = argparse.ArgumentParser(description="Sincroniza dos carpetas y registra las acciones.")
+    parser.add_argument("source", type=str, help="Ruta a la carpeta fuente")
+    parser.add_argument("replica", type=str, help="Ruta a la carpeta réplica")
+    parser.add_argument("interval", type=int, help="Intervalo en minutos (1-60)")
+    parser.add_argument("logFile", type=str, help="Ruta al archivo de registro")
+
+    args = parser.parse_args()
+
+    # Verifica si las rutas de source y replica existen
+    if not os.path.isdir(args.source):
+        sys.exit(f"Error: La carpeta fuente '{args.source}' no existe.")
+    if not os.path.isdir(args.replica):
+        sys.exit(f"Error: La carpeta réplica '{args.replica}' no existe.")
+    if args.interval < 1 or args.interval > 60:
+        sys.exit(f"Error: interval debe estar entre 1 y 60.")
+    pathSource = args.source
+    pathReplica = args.replica
+    fileToLog = open(args.logFile, "w")
+
+def readFoldersContent():
+    global pathSource, pathReplica, pathSourceContent, pathReplicaContent
+    pathSourceContent = readFolderContent(pathSource, pathSource)
+    pathReplicaContent = readFolderContent(pathReplica, pathReplica)
+    
+def readFolderContent(rootPath, path, folders=None, files=None):
+    if folders is None:
+        folders = []
+    if files is None:
+        files = []
+    content = os.listdir(path)
+    for item in content:
+        full_item_path = os.path.join(path, item)
+        relative_item_path = getRelativePath(rootPath, full_item_path)
+        if os.path.isdir(full_item_path):
+            folders.append(relative_item_path)
+            readFolderContent(rootPath, full_item_path, folders, files)
+        elif os.path.isfile(full_item_path):
+            files.append(relative_item_path)
+    return {"folders": folders, "files": files}
+
+def getRelativePath(rootPath, path):
+    return path.replace(rootPath, '')
+
+def createFoldersIfNoExistsInReplicaFromSorurce():    
+    global pathSourceContent, pathReplicaContent, pathReplica
+    for folder in pathSourceContent["folders"]:
+        if folder not in pathReplicaContent["folders"]:
+            os.makedirs(pathReplica + folder)
+            log(f"Create folder from source: {pathSource}{folder} to replica: {pathReplica}{folder}")
+
+def removeFoldersIfNoExistInSourceFromReplica():
+    global pathSourceContent, pathReplicaContent, pathReplica
+    for folder in pathReplicaContent["folders"]:
+        if folder not in pathSourceContent["folders"]:
+            try:
+                shutil.rmtree(pathReplica + folder)
+                log(f"Remove folder from replica: {pathReplica}{folder}")
+            except Exception:
+                pass
+
+def copyFilesIfNoExistsInReplicaFromSorurce():
+    global pathSourceContent, pathReplicaContent, pathReplica, pathSource
+    for file in pathSourceContent["files"]:
+        if file not in pathReplicaContent["files"]:
+            shutil.copyfile(pathSource + file, pathReplica + file)
+            log(f"Copy file from source: {pathSource}{file} to replica: {pathReplica}{file}")
+
+def removeFilesIfNoExistsInSourceFromReplica():
+    global pathSourceContent, pathReplicaContent, pathReplica, pathSource
+    for file in pathReplicaContent["files"]:
+        if file not in pathSourceContent["files"]:
+            try:
+                os.remove(pathReplica + file)
+                log(f"Remove file from replica: {pathReplica}{file}")
+            except Exception:
+                pass
+
+def copyFilesIfContentIsDiferentInReplicaFromSorurce():
+    for file in pathReplicaContent["files"]:
+        try:
+            if (calcular_md5(pathSource + file) != calcular_md5(pathReplica + file)):
+                shutil.copyfile(pathSource + file, pathReplica + file)
+                log(f"Copy content file from source: {pathSource}{file} to replica: {pathReplica}{file}")
+        except Exception:
+            pass
+
+def calcular_md5(path_file):
+    hash_md5 = hashlib.md5()
+    with open(path_file, "rb") as f:
+        for bloque in iter(lambda: f.read(4096), b""):
+            hash_md5.update(bloque)
+        f.flush()
+    return hash_md5.hexdigest()
+
+def log(text):
+    global fileToLog
+    print(text)
+    fileToLog.write(f"{text}\n")
+    fileToLog.flush()
+
+if __name__ == "__main__":
+    pathSource = ""
+    pathReplica = ""
+    fileToLog = None
+    pathSourceContent = {"folders": [], "files": []}
+    pathReplicaContent = {"folders": [], "files": []}
+    interval = 10
+    numberOfExecution = 0
+    controlArguments()
+    while True:
+        numberOfExecution += 1
+        print(f"Execution number: {numberOfExecution}" + str())
+        readFoldersContent()
+        createFoldersIfNoExistsInReplicaFromSorurce()
+        removeFoldersIfNoExistInSourceFromReplica()
+        copyFilesIfNoExistsInReplicaFromSorurce()
+        removeFilesIfNoExistsInSourceFromReplica()
+        copyFilesIfContentIsDiferentInReplicaFromSorurce()
+        time.sleep(interval)    
